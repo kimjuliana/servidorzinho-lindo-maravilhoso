de42061a8d1d122a6374afc19294d113f694e4a1
kimjuliana <juliana.kg@gmail.com>
2019-05-30 21:49:17 -0300

    Instalando cors, criando controller

--- server.js
+++ server.js
@@ -1,39 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+const controller = require('./ComidasController')
 
-const comidas = {
-    pratosFavoritos: [
-    {
-    nome: "Paçoquita",
-    descricao: "Doce",
-    imagem: "https://http2.mlstatic.com/pacoquita-20g-santa-helena-D_NQ_NP_540215-MLB25162744484_112016-F.jpg"
-},
-{
-    nome: "Batata frita",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/Batata-frita.jpg"
-  },
-  {
-    nome: "Macarronada",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/macarronada.jpg"
-  },
-  {
-    nome: "Falafel",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/falafel.jpg"
-  },
-  {
-    nome: "Creme de abóbora",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/creme-de-abobora.jpg"
-  },
-]
-}
+// controller.get()
+
+
 
 const servidor = express()
+servidor.use(cors())
 
 servidor.get("/comidas", (request, response) => {
-  response.send("Boa noite, amigas!")
+  // response.header('Access-Control-Allow-Origin', '*')
+  response.send(controller.getAll())
 })
 
 
