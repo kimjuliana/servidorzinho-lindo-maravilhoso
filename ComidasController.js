2f3fc65c9e61e40136b0df173faddda83de22f93
kimjuliana <juliana.kg@gmail.com>
2019-05-31 22:35:20 -0300

    metodo delete, criando id random

--- ComidasController.js
+++ ComidasController.js
@@ -1,39 +1,22 @@
+const repository = require('./ComidasRepository')
 const comidas = {
-    pratosFavoritos: [
-    {
-    nome: "Paçoquita",
-    descricao: "Doce",
-    imagem: "https://http2.mlstatic.com/pacoquita-20g-santa-helena-D_NQ_NP_540215-MLB25162744484_112016-F.jpg"
-},
-{
-    nome: "Batata frita",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/Batata-frita.jpg"
-  },
-  {
-    nome: "Macarronada",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/macarronada.jpg"
-  },
-  {
-    nome: "Falafel",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/falafel.jpg"
-  },
-  {
-    nome: "Creme de abóbora",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/creme-de-abobora.jpg"
-  },
-]
+    pratosFavoritos: []
 }
-
 
 
 const getAll = () => {
-    return comidas
+    return repository.comidas
 }
 const add = (comida) => {
-    comidas.pratosFavoritos.push(comida)
+    comida.id = Math.random().toString(36).substr(-8)
+    getAll().pratosFavoritos.push(comida)
 }
-module.exports = {getAll, add}
+
+const remove = (id) => {
+  let comidasRestantes = getAll()
+
+  getAll().pratosFavoritos = comidasRestantes.pratosFavoritos.filter((comida) => {
+    return comida.id !== id
+  })
+}
+module.exports = {comidas, getAll, add, remove}
--- server.js
+++ server.js
@@ -9,15 +9,21 @@ const controller = require('./ComidasController')
 
 const servidor = express()
 servidor.use(cors())
+servidor.use(bodyParser.json()) 
 
 servidor.get("/comidas", (request, response) => {
   // response.header('Access-Control-Allow-Origin', '*')
   response.send(controller.getAll())
 })
 
-servidor.post('/comidas', bodyParser.json(), (request, response) => {
+servidor.post("/comidas", (request, response) => {
   controller.add(request.body)
   response.send(201)
+})
+
+servidor.delete("/comidas/:id", (request,response) => {
+  controller.remove(request.params.id)
+  response.sendStatus(204)
 })
 
 servidor.listen (3000) //falando para o servidor acessar a porta escolhida 3mil p cima melhor
